ca3c17461986d2fa6c1dae4fd259d59066d5c015
unknown <ernest.obihara@icloud.com>
2021-01-09 19:42:18 +0000

    working template with netlify forms

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 
 export default function ContactForm() {
   return (
-    <div>
+    <div className="form-container">
       <h1>
         I'm a contact form<span className="fancy">.</span>
       </h1>
@@ -16,23 +16,67 @@ export default function ContactForm() {
         <input type="hidden" name="personal-site-conact" value="contact" />
         <p>
           <label>
-            Name: <input type="text" name="name" />
+            <span>Name</span> <input type="text" name="name" className="input-box"/>
           </label>
         </p>
         <p>
           <label>
-            Email: <input type="email" name="email" />
+          <span>Email</span> <input type="email" name="email" className="input-box"/>
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message"></textarea>
+          <span>Message</span> <textarea name="message" className="input-box"></textarea>
           </label>
         </p>
-        <p>
+        <p className="button-p">
           <button type="submit">Send</button>
         </p>
       </form>
+      <style jsx>{`
+        
+        .input-box {
+          margin : 0.5rem 1.5rem;
+          padding: 0.5rem 0;
+          width: 100%;
+          border 2px solid;
+          border-radius: 4px;
+        }
+        .button-p {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        form {
+          width: 500px;
+        }
+        label {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        textarea {
+          height 70px;
+        }
+        span {
+          margin-right: auto;
+        }
+
+        button {
+          background: black;
+          color: white;
+          padding: 0.5rem 1rem;
+          border-radius: 4px;
+          border-style: none;
+        }
+        @media (max-width: 769px) {
+          form {
+            width: 100%;
+          }
+        }
+      `}</style>
     </div>
   );
 }
